272bb1a9c90c5d04f4ceebb85830181a85fbf0d9
eric kyania <erickyania0@gmail.com>
2023-06-03 13:46:00 +0300

    created card and button components and used them

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,18 +1,25 @@
+import Button from "../UI/Button";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
-    const addUserHandler = event => {event.preventDefault()};
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
 
-   return(
-    <form onsubmit={addUserHandler}>
-        <label htmlFor="username" >Username</label>
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
         <input id="username" type="text"></input>
 
         <label htmlFor="age">Age</label>
         <input id="age" type="number"></input>
 
-        <button type="submit">Add User</button>
-    </form>
-   );
-}
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
+};
 
 export default AddUser;
